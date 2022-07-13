abffb6296ea7a4d9e1d7348e2065fb6a4991f1bb
Julian Martinez <julian.fmartinez@gmail.com>
2022-07-12 22:11:57 -0300

    Componentes Lazy Agregados

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import logo from '../logo.svg';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
 
 export const Navigation = () => {
   return (
@@ -9,21 +11,36 @@ export const Navigation = () => {
           <img src={logo} alt="React - Logo" />
           <ul>
             <li>
-              <NavLink to="/home" className={ ({isActive}) => isActive ? 'nav-active': ""}>Home</NavLink>
+              <NavLink
+                to="/lazy1"
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+              >
+                Lazy 1
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about" className={ ({isActive}) => isActive ? 'nav-active': ""}>About</NavLink>
+              <NavLink
+                to="/lazy2"
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+              >
+                Lazy 2
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/users" className={ ({isActive}) => isActive ? 'nav-active': ""}>Users</NavLink>
+              <NavLink
+                to="/lazy3"
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+              >
+                Lazy 3
+              </NavLink>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="about" element={<h1>About page</h1>} />
-          <Route path="users" element={<h1>Users page</h1>} />
-          <Route path="home" element={<h1>Home page</h1>} />
+          <Route path="lazy1" element={<LazyPage1 />} />
+          <Route path="lazy2" element={<LazyPage2 />} />
+          <Route path="lazy3" element={<LazyPage3 />} />
           <Route path="/*" element={<h1>404</h1>} />
         </Routes>
       </div>
